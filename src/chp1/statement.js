3edce6cdc9e1fd58481d433d1943f6e9a3e3c899
hn.lee <hn.lee@twolinecode.com>
2021-10-01 19:07:18 +0900

    ing

--- src/chp1/statement.js
+++ src/chp1/statement.js
@@ -1,40 +1,61 @@
 export {statement}
 
+
 function statement(invoice, plays) {
+  // 총합
   let totalAmount = 0;
+
   let volumeCredits = 0;
   let result = `청구 내역 (고객명: ${invoice.customer}\n`
-  const format = new Intl.NumberFormat('euc-en', {style: 'currency', currency: 'USD', minimumFractionDigits: 2}).format;
+  const format = new Intl.NumberFormat('en-US', {style: 'currency', currency: 'USD', minimumFractionDigits: 2}).format;
+
   for (let perf of invoice.performances) {
-    const play = plays[perf.playID];
-    let thisAmount = 0;
-    switch (play.type) {
-      case 'tragedy' :
-        thisAmount = 40000;
-        if (perf.audience > 30) {
-          thisAmount += 1000 * (perf.audience - 30);
-        }
-        break;
-      case 'comedy':
-        thisAmount = 30000;
-        if (perf.audience > 20) {
-          thisAmount += 10000 + 500 * (perf.audience - 20);
-        }
-        thisAmount += 300 * perf.audience;
-        break;
-      default:
-        throw new Error(`알 수 없는 장르: ${play.type}`);
-    }
+    // 포인트 적립
+    volumeCredits += volumeCreditsFor(perf);
 
-    volumeCredits += Math.max(perf.audience - 30, 0);
-
-    if ('comedy' === play.type) volumeCredits += Math.floor(perf.audience / 5);
-
-    result += `  ${play.name}: ${format(thisAmount / 100)} (${perf.audience}석)\n`;
-    totalAmount += thisAmount;
+    // 청구 내역 출력
+    result += `  ${playFor(perf).name}: ${format(amountFor(perf) / 100)} (${perf.audience}석)\n`;
+    totalAmount += amountFor(perf);
   }
 
   result += `총액: ${format(totalAmount / 100)}\n`
   result += `적립 포인트: ${volumeCredits}점\n`
+
+
   return result;
+
+  function amountFor(aPerformance) {
+    let result = 0;
+    switch (playFor(aPerformance).type) {
+      case 'tragedy' :
+        result = 40000;
+        if (aPerformance.audience > 30) {
+          result += 1000 * (aPerformance.audience - 30);
+        }
+        break;
+      case 'comedy':
+        result = 30000;
+        if (aPerformance.audience > 20) {
+          result += 10000 + 500 * (aPerformance.audience - 20);
+        }
+        result += 300 * aPerformance.audience;
+        break;
+      default:
+        throw new Error(`알 수 없는 장르: ${playFor(aPerformance).type}`);
+    }
+    return result;
+  }
+
+  function playFor(perf) {
+    return plays[perf.playID];
+  }
+
+  function volumeCreditsFor(perf) {
+    let volumeCredits = 0;
+    Math.max(perf.audience - 30, 0);
+    if ('comedy' === playFor(perf).type)
+      volumeCredits += Math.floor(perf.audience / 5);
+    return volumeCredits;
+  }
+
 }
